118cbf0946d00883a09362c855fc2f83f9c7451f
ArtemAlieinikov <artemalieinikov@gmail.com>
2016-12-26 14:56:04 +0200

    Added additional test class.

---
+++ Getting-state-through-reflection/ColorLessClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_state_through_reflection
+{
+    class ColorLessClass
+    {
+        [PropertyColor(ConsoleColor.DarkBlue)]
+        private int DarkBlueProp { get; set; }
+
+        [PropertyColor(ConsoleColor.DarkCyan)]
+        private int DarkCyanProp { get; set; }
+
+        [PropertyColor(ConsoleColor.DarkGray)]
+        private int DarkGrayProp { get; set; }
+
+        [PropertyColor(ConsoleColor.DarkYellow)]
+        private int DarkYellowProp { get; set; }
+
+        public ColorLessClass()
+        { }
+
+        public ColorLessClass(int blue, int cyan, int gray, int yellow)
+        {
+            DarkBlueProp = blue;
+            DarkCyanProp = cyan;
+            DarkGrayProp = gray;
+            DarkYellowProp = yellow;
+        }
+    }
+}
--- Getting-state-through-reflection/Program.cs
+++ Getting-state-through-reflection/Program.cs
@@ -11,7 +11,9 @@ namespace Getting_state_through_reflection
         static void Main(string[] args)
         {
             ColorFullClass test = new ColorFullClass(100, "Hi", "How are you?", true, 18.5, new object(), 'X');
+            ColorLessClass test1 = new ColorLessClass(50, 1, 0, 80);
             ClassProperties.PropertyValueColorPrint(test);
+            ClassProperties.PropertyValueColorPrint(test1);
             //ClassProperties.PropertyInfoColorPrint(test);
         }
     }
